9e06c08374046830ecc91a32410a63a52cff292e
adithyan-r1281 <adithyanr12801@gmail.com>
2022-12-07 16:15:49 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import {useState} from 'react'
 function App() {
+  const[counter,setCounter]=useState(0)//counter is a state to hold values
+  function incrementCounter(){
+    setCounter(counter+1);
+  }
+  function decrement(){
+    if(counter>0){
+      setCounter(counter-1)
+
+    }
+  }
+  function reset(){
+    setCounter(0)
+  }
   return (
-    <div className="App">
+    <div style={{display:'flex',flexDirection:'column',alignItems:'center',justifyContent:'center',margin:'10px',padding:'2px 5px'}} className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3 style={{color:'white',backgroundColor:'red',padding:'5px',textAlign:'center'}}>Counter App</h3>
+        <h2 style={{color:'red',textAlign:'center'}}>{counter}</h2>
+        <button style={{backgroundColor:"green",color:"white",padding:"10px",margin:'5px',borderRadius:'25%',borderColor:'white'}} onClick={incrementCounter}>ADD</button>
+        <button style={{backgroundColor:"red",color:"white",padding:"10px",margin:'5px',borderRadius:'25%',borderColor:'white'}} onClick={decrement}>REDUCE</button>
+        <button style={{backgroundColor:"blue",color:"white",padding:"10px",margin:'5px',borderRadius:'25%',borderColor:'white'}} onClick={reset}>RESET</button>
+
       </header>
     </div>
   );
